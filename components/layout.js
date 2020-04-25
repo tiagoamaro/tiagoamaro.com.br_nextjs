814e86ff84c87f8546fd57f298f6d408a804f447
Tiago Amaro <tiagopadrela@gmail.com>
2020-04-25 17:26:09 -0300

    Import icons

--- components/layout.js
+++ components/layout.js
@@ -7,6 +7,9 @@ export default function Theme ({ children }) {
       <Head>
         <title>Tiago Padrela Amaro</title>
         <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+        {/* Icons */}
+        <link rel='apple-touch-icon-precomposed' sizes='144x144' href='/icons/touch-icon-144-precomposed.png' />
+        <link rel='shortcut icon' href='/icons/favicon.png' />
       </Head>
       <NavBar />
       {children}
