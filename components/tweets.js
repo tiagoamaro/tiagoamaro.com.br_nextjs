9dec709674179acd57f87f003bcaca82ca787b50
Tiago Amaro <tiagopadrela@gmail.com>
2020-04-25 21:57:55 -0300

    Embed tweets

--- components/tweets.js
+++ components/tweets.js
@@ -1,3 +1,20 @@
+import { useEffect } from 'react'
+
 export default function Tweets () {
-  return <>Tweets</>
+  useEffect(() => {
+    const anchor = document.createElement('a')
+    anchor.setAttribute('class', 'twitter-timeline')
+    anchor.setAttribute('data-theme', 'dark')
+    anchor.setAttribute('data-dnt', 'true')
+    anchor.setAttribute('href', 'https://twitter.com/tiagopadrela')
+    document.getElementsByClassName('twitter-embed')[0].appendChild(anchor)
+
+    const script = document.createElement('script')
+    script.setAttribute('src', 'https://platform.twitter.com/widgets.js')
+    document.getElementsByClassName('twitter-embed')[0].appendChild(script)
+  }, [])
+
+  return (
+    <div className='twitter-embed' />
+  )
 }
