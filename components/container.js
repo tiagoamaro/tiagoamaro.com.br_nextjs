707d78c555f64ccff45794c995659315edbb90d5
Tiago Amaro <tiagopadrela@gmail.com>
2020-04-25 21:22:46 -0300

    About page with style adjustments

--- components/container.js
+++ components/container.js
@@ -1,9 +1,9 @@
 export default function Container ({ children }) {
   return (
-    <div class='pure-g'>
-      <div class='pure-u-1 pure-u-lg-1-5' />
-      <div class='pure-u-1 pure-u-lg-3-5'>{children}</div>
-      <div class='pure-u-1 pure-u-lg-1-5' />
+    <div className='pure-g'>
+      <div className='pure-u-lg-1-5 pure-u-1-24' />
+      <div className='pure-u-lg-3-5 pure-u-22-24'>{children}</div>
+      <div className='pure-u-lg-1-5 pure-u-1-24' />
     </div>
   )
 }
--- components/navbar.js
+++ components/navbar.js
@@ -2,19 +2,19 @@ import Link from 'next/link'
 
 export default function NavBar () {
   return (
-    <nav class='pure-g'>
-      <div class='pure-u-1 pure-u-lg-1-5 text-center berkshire'>Tiago Padrela Amaro</div>
-      <div class='pure-u-1 pure-u-lg-1-5 text-center'>
-        <Link href='/'>Tweets</Link>
+    <nav className='pure-g'>
+      <div className='pure-u-1 pure-u-lg-1-5 berkshire'>Tiago Padrela Amaro</div>
+      <div className='pure-u-1 pure-u-lg-1-5 text-center'>
+        <Link href='/'><a>Tweets</a></Link>
       </div>
-      <div class='pure-u-1 pure-u-lg-1-5 text-center'>
-        <Link href='/posts'>Posts</Link>
+      <div className='pure-u-1 pure-u-lg-1-5 text-center'>
+        <Link href='/posts'><a>Posts</a></Link>
       </div>
-      <div class='pure-u-1 pure-u-lg-1-5 text-center'>
-        <Link href='/about'>About</Link>
+      <div className='pure-u-1 pure-u-lg-1-5 text-center'>
+        <Link href='/about'><a>About</a></Link>
       </div>
-      <div class='pure-u-1 pure-u-lg-1-5 text-center'>
-        <Link href='/quotes'>Quotes</Link>
+      <div className='pure-u-1 pure-u-lg-1-5 text-center'>
+        <Link href='/quotes'><a>Quotes</a></Link>
       </div>
     </nav>
   )
