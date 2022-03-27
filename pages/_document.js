e1ce7ed1ff7aa5aeffd360cb9a9cfa3539bbd828
Tiago Amaro <tiagopadrela@gmail.com>
2022-03-27 11:42:25 -0300

    Creating favicon images

--- pages/_document.js
+++ pages/_document.js
@@ -9,6 +9,13 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Berkshire+Swash&display=optimal"
             rel="stylesheet"
           />
+
+          <link rel="shortcut icon" href="/favicon.png" />
+          <link
+            rel="apple-touch-icon-precomposed"
+            sizes="144x144"
+            href="/touch-icon-144-precomposed.png"
+          ></link>
         </Head>
         <body>
           <Main />
