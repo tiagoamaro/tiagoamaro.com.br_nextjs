3e690af7bb163265b91c36be10233dc7a3fd0dd5
Tiago Amaro <tiagopadrela@gmail.com>
2022-03-28 08:56:26 -0300

    WIP about page

---
+++ pages/about.js
@@ -0,0 +1,27 @@
+import { NextSeo } from "next-seo"
+
+export const About = () => {
+  return (
+    <>
+      <NextSeo title="About" />
+
+      <h1 className="mt-0 mb-2 text-5xl font-medium leading-snug">About</h1>
+      <p>Hi,</p>
+      <p>
+        My name is Tiago, and I&apos;m a brazilian developer living in Rio de
+        Janeiro. Writing better software that people will love is my motto, and
+        I&apos;m always seeking new talents. I&apos;m focused in web
+        technologies and my main programming language is Ruby.
+      </p>
+      <p>I am currently a developer for The Conversation.</p>
+
+      <h2 className="text-2xl font-medium leading-relaxed">
+        Where to find me?
+      </h2>
+
+      <p>TODO</p>
+    </>
+  )
+}
+
+export default About
