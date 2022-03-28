b9ad1ed08e52432a22b62ce17c3344185979c217
Tiago Amaro <tiagopadrela@gmail.com>
2022-03-28 16:20:42 -0300

    Create quotes page

---
+++ pages/quotes.js
@@ -0,0 +1,289 @@
+import { NextSeo } from "next-seo"
+
+export const Quotes = () => {
+  return (
+    <>
+      <NextSeo title="Quotes" />
+
+      <div className="border-l-2 border-gray-300 pl-4">
+        <blockquote>
+          The mark of a person who is in control of consciousness is the ability
+          to focus attention at will,
+          <br />
+          to be oblivious to distractions, to concentrate for as long as it
+          takes to achieve a goal, and not longer.
+          <br />
+          And the person who can do this usually enjoys the normal course of
+          everyday life.
+          <br />
+          <small>
+            <cite>Mihaly Csikszentmihalyi, Flow</cite>
+          </small>
+        </blockquote>
+      </div>
+
+      <hr className="my-4" />
+
+      <div className="border-l-2 border-gray-300 pl-4">
+        <blockquote>
+          You can do a lot in a lifetime
+          <br />
+          If you don’t burn out too fast
+          <br />
+          You can make the most of the distance
+          <br />
+          First you need endurance —<br />
+          First you’ve got to last…
+          <br />
+          <small>
+            <cite>Neil Peart (Marathon - Rush)</cite>
+          </small>
+        </blockquote>
+      </div>
+
+      <hr className="my-4" />
+
+      <div className="border-l-2 border-gray-300 pl-4">
+        <blockquote>
+          Nous sommes des nains juchés sur les épaules de géants. <br />
+          Nous voyons ainsi davantage et plus loin qu'eux, non parce que notre
+          vue est plus aiguë ou notre taille plus haute, mais parce qu'ils nous
+          portent en l'air et nous élèvent de toute leur hauteur gigantesque.{" "}
+          <br />
+          (We are dwarfs perched on the shoulders of giants, <br /> so we see
+          more and more than they do, not because our sight is sharper or our
+          height higher, but because they carry us up and we raise their
+          gigantic height) <br />
+          <small>
+            <cite>Bernard de Chartres</cite>
+          </small>
+        </blockquote>
+      </div>
+
+      <hr className="my-4" />
+
+      <div className="border-l-2 border-gray-300 pl-4">
+        <blockquote>
+          You can catch a monkey by burying a narrow-mouthed jar of nuts in the
+          ground. <br />
+          A monkey comes along, pits his paw into the jar and grabs a handful of
+          nuts, but the mouth of the jar is too narrow to let him withdraw his
+          clenched fist. <br />
+          The monkey is unwilling to let go of the nuts and so is trapped.{" "}
+          <br />
+          You too can be trapped by not letting go of your beliefs, opinions,
+          worries and anxieties. <br />
+          <small>
+            <cite>Michael Michalko - Thinkertoys</cite>
+          </small>
+        </blockquote>
+      </div>
+
+      <hr className="my-4" />
+
+      <div className="border-l-2 border-gray-300 pl-4">
+        <blockquote>
+          The quality of our mental models determines whether the code we write
+          does what we expect it to do. <br />
+          And that translates directly into avoiding bugs and meeting deadlines.{" "}
+          <br />
+          The more effective our models, the more work we're able to get done.{" "}
+          <br />
+          <small>
+            <cite>Avdi Grimm</cite>
+          </small>
+        </blockquote>
+      </div>
+
+      <hr className="my-4" />
+
+      <div className="border-l-2 border-gray-300 pl-4">
+        <blockquote>
+          Don't write things you don't believe. <br />
+          And write them carefully. <br />
+          And write them knowing that, you're not to write them unless you're
+          willing to stand behind them. <br />
+          Then understand that if you're called to stand behind them, <br />
+          That is in fact how you will become educated. <br />
+          <small>
+            <cite>Jordan Peterson</cite>
+          </small>
+        </blockquote>
+      </div>
+
+      <hr className="my-4" />
+
+      <div className="border-l-2 border-gray-300 pl-4">
+        <blockquote>
+          Bugs are like water leaks. If you leave them too long, they can spoil
+          your merchandise and slow down your business.
+          <br />
+          <small>
+            <cite>Sam Hatoum</cite>
+          </small>
+        </blockquote>
+      </div>
+
+      <hr className="my-4" />
+
+      <div className="border-l-2 border-gray-300 pl-4">
+        <blockquote>
+          Fear is the path to the dark side. <br />
+          Fear leads to anger. <br />
+          Anger leads to hate. <br />
+          Hate leads to suffering. <br />
+          <small>
+            <cite>Master Yoda - Star Wars Universe</cite>
+          </small>
+        </blockquote>
+      </div>
+
+      <hr className="my-4" />
+
+      <div className="border-l-2 border-gray-300 pl-4">
+        <blockquote>
+          No code runs faster than no code. <br />
+          No code has fewer bugs than no code. <br />
+          No code uses less memory than no code. <br />
+          No code is easier to understand than no code. <br />
+          <small>
+            <cite>Mike Perham</cite>
+          </small>
+        </blockquote>
+      </div>
+
+      <hr className="my-4" />
+
+      <div className="border-l-2 border-gray-300 pl-4">
+        <blockquote>
+          Beautiful is better than ugly.
+          <br />
+          Explicit is better than implicit.
+          <br />
+          Simple is better than complex.
+          <br />
+          Complex is better than complicated.
+          <br />
+          Flat is better than nested.
+          <br />
+          Sparse is better than dense.
+          <br />
+          Readability counts.
+          <br />
+          Special cases aren't special enough to break the rules.
+          <br />
+          Although practicality beats purity.
+          <br />
+          Errors should never pass silently.
+          <br />
+          Unless explicitly silenced.
+          <br />
+          In the face of ambiguity, refuse the temptation to guess.
+          <br />
+          There should be one-- and preferably only one --obvious way to do it.
+          <br />
+          Although that way may not be obvious at first unless you're Dutch.
+          <br />
+          Now is better than never.
+          <br />
+          Although never is often better than *right* now.
+          <br />
+          If the implementation is hard to explain, it's a bad idea.
+          <br />
+          If the implementation is easy to explain, it may be a good idea.
+          <br />
+          Namespaces are one honking great idea -- let's do more of those!
+          <br />
+          <small>
+            <cite>The Zen of Python, by Tim Peters</cite>
+          </small>
+        </blockquote>
+      </div>
+
+      <hr className="my-4" />
+
+      <div className="border-l-2 border-gray-300 pl-4">
+        <blockquote>
+          Examine and interrogate your motivations, reject the money if you
+          dare, and startup something useful. Curb your ambition. Live happily
+          ever after.
+          <br />
+          <small>
+            <cite>David Heinemeier Hansson</cite>
+          </small>
+        </blockquote>
+      </div>
+
+      <hr className="my-4" />
+
+      <div className="border-l-2 border-gray-300 pl-4">
+        <blockquote>
+          Felicidade é somente acordar quando os olhos abrem.
+          <br />
+          (Happiness is waking up only when your eyes open.)
+          <br />
+          <small>
+            <cite>Julia Gostkorzewicz</cite>
+          </small>
+        </blockquote>
+      </div>
+
+      <hr className="my-4" />
+
+      <div className="border-l-2 border-gray-300 pl-4">
+        <blockquote>
+          Any fool can write code that a computer can understand. Good
+          programmers write code that humans can understand.
+          <br />
+          <small>
+            <cite>Martin Fowler</cite>
+          </small>
+        </blockquote>
+      </div>
+
+      <hr className="my-4" />
+
+      <div className="border-l-2 border-gray-300 pl-4">
+        <blockquote>
+          Building an object-oriented application means inventing appropriate
+          machinery. We represent real-world information, processes,
+          interactions, relationships, even errors, by inventing objects that
+          don't exist in the real world. We give life and intelligence to
+          inanimate things. We take difficult-to-comprehend real-world objects
+          and split them into simpler, more manageable software ones. We invent
+          new objects. Each has a specific role to play in the application. Our
+          measure of success lies in how clearly we invent a software reality
+          that satisfies our application's requirements -- and not in how
+          closely it remembers the real world.
+          <br />
+          <small>
+            <cite>Rebecca Wirfs-Brock, Object Design</cite>
+          </small>
+        </blockquote>
+      </div>
+
+      <hr className="my-4" />
+
+      <div className="border-l-2 border-gray-300 pl-4">
+        <blockquote>
+          If we accept that we reflect values into code whether we want or not,
+          we can't look into the monitor. We have to look into the mirror. If we
+          can be mindful of the values we reflect into our code; if we can
+          approach our craft with humility and an understanding of our
+          limitations; if we can admit that cleverness is no substitute for
+          compassion; if we can acknowledge that we always have a perspective
+          and it is only one of many perspectives; if we can embrace ambiguity;
+          if we can view our designs as a conversation rather than a
+          declaration; if we can bring these human values to our code, we stand
+          a chance of bringing soul to software.
+          <br />
+          <small>
+            <cite>Avdi Grimm, The Soul of Software</cite>
+          </small>
+        </blockquote>
+      </div>
+    </>
+  )
+}
+
+export default Quotes
